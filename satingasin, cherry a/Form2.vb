69e3d90e0deab8183579491e616af4d90699653a
Satingasin24 <135184248+Satingasin24@users.noreply.github.com>
2023-06-13 11:16:01 +0800

    Add files via upload

---
+++ satingasin, cherry a/Form2.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'UWIEFFSU
+        If My.Settings.SerialKey = TextBox1.Text Then
+            My.Settings.AppStat = "Full Version"
+            My.Settings.Save()
+            Me.Close()
+        Else
+            MsgBox("Invalid Serial Key")
+        End If
+    End Sub
+End Class
